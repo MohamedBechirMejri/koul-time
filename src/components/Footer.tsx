d9b0652e5b6f045de27e8b39966e229926c1f1df
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-03-16 10:02:34 +0100

    add footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,49 @@
 const Footer = () => {
   return (
-    <div className="mt-8 h-24 bg-[#FF5400] p-4">
+    <div className="mt-8 flex h-24 flex-col items-center justify-center gap-2 bg-[#FF5400] p-4">
       <h1>Connect with us</h1>
-      <div></div>
+      <div className="flex justify-center gap-4 text-white">
+        <a
+          href="https://www.youtube.com/c/KoulTime"
+          target="_blank"
+          rel="noreferrer"
+          className="transition-all hover:underline"
+        >
+          YouTube
+        </a>
+        <a
+          href="https://www.instagram.com/koul_time/"
+          target="_blank"
+          rel="noreferrer"
+          className="transition-all hover:underline"
+        >
+          Instagram
+        </a>
+        <a
+          href="https://www.tiktok.com/@koul_time"
+          target="_blank"
+          rel="noreferrer"
+          className="transition-all hover:underline"
+        >
+          Tiktok
+        </a>
+        <a
+          href="https://www.facebook.com/KoulTimeTV"
+          target="_blank"
+          rel="noreferrer"
+          className="transition-all hover:underline"
+        >
+          Facebook
+        </a>
+        <a
+          href="mailto:contact.koultime@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+          className="transition-all hover:underline"
+        >
+          Email
+        </a>
+      </div>
     </div>
   );
 };
