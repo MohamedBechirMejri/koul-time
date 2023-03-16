e58f1e987adbc2a426a87f7775cf6ad313e61fc9
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-03-16 09:39:06 +0100

    add sponsors section

---
+++ src/components/Sponsors.tsx
@@ -0,0 +1,14 @@
+const Sponsors = () => {
+  return (
+    <div className="grid grid-cols-2 grid-rows-[auto,7rem,7rem,7rem] gap-2 p-4">
+      <h1 className="col-span-2 text-3xl">Sponsors</h1>
+      <div className="col-span-2 bg-black"></div>
+      <div className="bg-black"></div>
+      <div className="bg-black"></div>
+      <div className="bg-black"></div>
+      <div className="bg-black"></div>
+    </div>
+  );
+};
+
+export default Sponsors;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Header from "~/components/Header";
 import LatestVideo from "~/components/LatestVideo";
+import Sponsors from "~/components/Sponsors";
 import TheCrew from "~/components/TheCrew";
 
 const Home: NextPage = () => {
@@ -16,6 +17,7 @@ const Home: NextPage = () => {
         <Header />
         <LatestVideo />
         <TheCrew />
+        <Sponsors />
       </div>
     </>
   );
