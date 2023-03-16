726650cfe44da06b6a8fa01b985dba33921f632c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-03-16 09:35:38 +0100

    add crew section

---
+++ src/components/TheCrew.tsx
@@ -0,0 +1,14 @@
+const TheCrew = () => {
+  return (
+    <div className="grid grid-cols-5 grid-rows-[auto,auto] gap-2 p-4">
+      <h1 className="col-span-5 text-3xl">The Crew</h1>
+      <div className="h-[7rem] bg-black"></div>
+      <div className="h-[7rem] bg-black"></div>
+      <div className="h-[7rem] bg-black"></div>
+      <div className="h-[7rem] bg-black"></div>
+      <div className="h-[7rem] bg-black"></div>
+    </div>
+  );
+};
+
+export default TheCrew;
