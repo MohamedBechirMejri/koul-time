daae6f180aca979596b9f767a91f8b21a84e5086
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-03-16 09:01:12 +0100

    add bangers font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,21 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
+import { Bangers } from "next/font/google";
+
 import "~/styles/globals.css";
 
+const bangers = Bangers({
+  variable: "--font-bangers",
+  subsets: ["latin"],
+  weight: "400",
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div style={bangers.style}>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default MyApp;
