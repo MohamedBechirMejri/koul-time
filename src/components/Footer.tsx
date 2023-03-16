fbcc940addb63cbaca6373fc7f96bc1b5b34c3e2
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-03-16 09:43:07 +0100

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="mt-8 h-24 bg-[#FF5400] p-4">
+      <h1>Connect with us</h1>
+      <div></div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import Footer from "~/components/Footer";
 import Header from "~/components/Header";
 import LatestVideo from "~/components/LatestVideo";
 import Sponsors from "~/components/Sponsors";
@@ -18,6 +19,7 @@ const Home: NextPage = () => {
         <LatestVideo />
         <TheCrew />
         <Sponsors />
+        <Footer />
       </div>
     </>
   );
