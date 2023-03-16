6f5a8820d8b38926a77799bf817e2cd6cf744221
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-03-16 10:47:03 +0100

    reduce cols

--- src/components/TheCrew.tsx
+++ src/components/TheCrew.tsx
@@ -1,8 +1,7 @@
 const TheCrew = () => {
   return (
-    <div className="grid grid-cols-5 grid-rows-[auto,auto] gap-2 p-4">
-      <h1 className="col-span-5 text-3xl">The Crew</h1>
-      <div className="h-[7rem] bg-black"></div>
+    <div className="grid grid-cols-4 grid-rows-[auto,auto] gap-2 p-4">
+      <h1 className="col-span-4 text-3xl">The Crew</h1>
       <div className="h-[7rem] bg-black"></div>
       <div className="h-[7rem] bg-black"></div>
       <div className="h-[7rem] bg-black"></div>
