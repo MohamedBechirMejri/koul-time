b5817060de156e39fb9e321bf5c97f784677ac24
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-03-17 01:57:48 +0100

    tweak height

--- src/components/TheCrew.tsx
+++ src/components/TheCrew.tsx
@@ -2,22 +2,22 @@ const TheCrew = () => {
   return (
     <div className="grid select-none grid-cols-4 grid-rows-[auto,auto] gap-2 p-4">
       <h1 className="col-span-4 text-3xl">The Crew</h1>
-      <div className="group h-[7rem] bg-black bg-[url(/avatars/melek.png)] bg-cover bg-center">
+      <div className="group h-[7rem] bg-black bg-[url(/avatars/melek.png)] bg-cover bg-center sm:h-[14rem]">
         <p className="flex h-full w-full items-center justify-center bg-black bg-opacity-25 text-2xl text-white opacity-0 transition-all group-hover:opacity-100">
           Melek
         </p>
       </div>
-      <div className="group h-[7rem] bg-black bg-[url(/avatars/saif.png)] bg-cover bg-center">
+      <div className="group bg-black bg-[url(/avatars/saif.png)] bg-cover bg-center">
         <p className="flex h-full w-full items-center justify-center bg-black bg-opacity-25 text-2xl text-white opacity-0 transition-all group-hover:opacity-100">
           Saif
         </p>
       </div>
-      <div className="group h-[7rem] bg-black bg-[url(/avatars/houssem.png)] bg-cover bg-center">
+      <div className="group bg-black bg-[url(/avatars/houssem.png)] bg-cover bg-center">
         <p className="flex h-full w-full items-center justify-center bg-black bg-opacity-25 text-2xl text-white opacity-0 transition-all group-hover:opacity-100">
           Houssem
         </p>
       </div>
-      <div className="group h-[7rem] bg-black bg-[url(/avatars/yahya.png)] bg-cover bg-center">
+      <div className="group bg-black bg-[url(/avatars/yahya.png)] bg-cover bg-center">
         <p className="flex h-full w-full items-center justify-center bg-black bg-opacity-25 text-2xl text-white opacity-0 transition-all group-hover:opacity-100">
           Yahia
         </p>
