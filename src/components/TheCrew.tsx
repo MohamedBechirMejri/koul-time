c1a4555f75303cdb041c5a7b6b5a2fe5dbf6b69c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-03-17 01:31:15 +0100

    add team photos

--- src/components/TheCrew.tsx
+++ src/components/TheCrew.tsx
@@ -2,10 +2,10 @@ const TheCrew = () => {
   return (
     <div className="grid grid-cols-4 grid-rows-[auto,auto] gap-2 p-4">
       <h1 className="col-span-4 text-3xl">The Crew</h1>
-      <div className="h-[7rem] bg-black"></div>
-      <div className="h-[7rem] bg-black"></div>
-      <div className="h-[7rem] bg-black"></div>
-      <div className="h-[7rem] bg-black"></div>
+      <div className="h-[7rem] bg-black bg-[url(/avatars/melek.png)] bg-cover bg-center"></div>
+      <div className="h-[7rem] bg-black bg-[url(/avatars/saif.png)] bg-cover bg-center"></div>
+      <div className="h-[7rem] bg-black bg-[url(/avatars/houssem.png)] bg-cover bg-center"></div>
+      <div className="h-[7rem] bg-black bg-[url(/avatars/yahya.png)] bg-cover bg-center"></div>
     </div>
   );
 };
